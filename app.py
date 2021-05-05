79077022688623dd61f138dd42859f3a2c89dc01
d-jenkins <75498733+d-jenkins@users.noreply.github.com>
2021-05-05 17:56:05 -0500

    Updated Flask app

--- app.py
+++ app.py
@@ -1,45 +1,24 @@
 # import dependencies
-import numpy as np
-
-import sqlalchemy
-from sqlalchemy.ext.automap import automap_base
-from sqlalchemy.orm import Session
-from sqlalchemy import create_engine, func
-
+import os
 from flask_pymongo import PyMongo
-
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, request, redirect
 
 # Import pgadmin password from python file
-from nbapw import pw
-
-#################################################
-# Create Postgres Database Connection
-#################################################
-engine = create_engine(f'postgresql://postgres:{pw}@localhost:5432/project_nba')
-#engine = create_engine(f'postgresql://{connection_string}')
-# reflect an existing database into a new model
-Base = automap_base()
-# reflect the tables
-Base.prepare(engine, reflect=True)
-
-# Create references to tables
-# placeholder = Base.classes.placeholder
-
-##########################################################
-# Connect to MongoDB
-##########################################################
-# app.config["MONGO_URI"] = "mongodb://localhost:27017/project_nba"
-# mongo = PyMongo(app)
-
+#from nbapw import pw
 
 
 ##########################################################
 # Setup Flask
 ##########################################################
-
 # Create an app
 app = Flask(__name__)
+##########################################################
+# Connect to MongoDB
+##########################################################
+#myclient = db.MongoClient("mongodb://localhost:27017/projectNBA")
+conn = app.config["MONGO_URI"] = "mongodb://localhost:27017/projectNBA"
+mongo = PyMongo(app)
+
 
 ##########################################################
 # Decorate Flask Routes
@@ -49,6 +28,16 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route('/ballers', methods=['GET'])
+def get_ballers():
+  ballers = mongo.db.nbaPlayers
+  output = []
+  for b in ballers.find():
+    output.append({'name' : b['name'], 'Page' : b['Page']})
+  return jsonify({'result' : output})
+
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
